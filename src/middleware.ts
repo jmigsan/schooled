e57f24ae6d3489d9d2e6f395d4a8c7ccec49e4a2
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-05-16 20:06:51 +0100

    made middleware normal

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// huh. this works.
-const myMiddleware = () => {
-  // console.log("does this work?");
-  return authMiddleware();
-};
-export default myMiddleware();
+export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
