4b9bbdecaea7dae0e2152bc572489bd7f9b3b6c9
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-05-19 10:52:45 +0100

    post and profile pages

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,29 @@
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { type NextPage } from "next";
+import Head from "next/head";
+import SignOutButton from "~/components/auth/SignOutButton";
+
+const Post: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Schooled - View Post</title>
+        <meta name="description" content="📚" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col gap-4 p-4">
+        <header>
+          <SignedIn>
+            <SignOutButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </header>
+        <section></section>
+      </main>
+    </>
+  );
+};
+
+export default Post;
