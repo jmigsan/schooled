7eb30b25e6e9de94dda18fe0dee05f95d543ecc7
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-05-19 20:24:20 +0100

    sign out maybe work now?

--- src/components/auth/SignOutButton.tsx
+++ src/components/auth/SignOutButton.tsx
@@ -2,7 +2,12 @@ import { useClerk } from "@clerk/nextjs";
 
 const SignOutButton = () => {
   const { signOut } = useClerk();
-  return <button onClick={() => signOut()}>Sign out</button>;
+
+  const clickSignOut = () => {
+    signOut();
+  };
+
+  return <button onClick={clickSignOut}>Sign out</button>;
 };
 
 export default SignOutButton;
