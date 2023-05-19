b02907f50471793ad824ed4684672ce47a141024
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-05-19 20:14:48 +0100

    old middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export default authMiddleware({
-  publicRoutes: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
 });
 
+// Stop Middleware running on static files
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
