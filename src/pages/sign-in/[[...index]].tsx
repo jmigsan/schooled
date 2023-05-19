c9f643a09c7facf139842f57998292f5ab805d4f
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-05-19 21:47:58 +0100

    sign in sign out pages

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -3,7 +3,9 @@ import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => {
   return (
     <div className="h-screen bg-gradient-to-br from-purple-500 to-blue-500">
-      <SignIn />
+      <div className="p-4">
+        <SignIn />
+      </div>
     </div>
   );
 };
