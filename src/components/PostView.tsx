03dc538401ceaff9374ceb055decb26cf05e1cf6
jmigsan <43961001+jmigsan@users.noreply.github.com>
2023-05-27 00:39:19 +0100

    no shadow

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -10,7 +10,7 @@ const PostView = (props: PostWithAuthor) => {
   const { author, post } = props;
 
   return (
-    <div className="rounded-2xl bg-red-400 p-4 text-white shadow-lg">
+    <div className="rounded-2xl bg-red-400 p-4 text-white">
       <div className="flex items-center gap-3 ">
         <Link href={`/${author.id}`}>
           <img
